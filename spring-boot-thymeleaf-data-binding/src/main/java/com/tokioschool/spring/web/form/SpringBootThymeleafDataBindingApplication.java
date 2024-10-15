790329c70fd287b470358a9a77bc870fe31e820a
Andrés Ruiz Peñuela <andres.rpenuela@babelgroup.com>
2024-10-15 13:35:06 +0200

    [udemy-sts6] init project empty

---
+++ spring-boot-thymeleaf-data-binding/src/main/java/com/tokioschool/spring/web/form/SpringBootThymeleafDataBindingApplication.java
@@ -0,0 +1,13 @@
+package com.tokioschool.spring.web.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafDataBindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafDataBindingApplication.class, args);
+	}
+
+}
---
+++ spring-boot-thymeleaf-data-binding/src/test/java/com/tokioschool/spring/web/form/SpringBootThymeleafDataBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokioschool.spring.web.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafDataBindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
