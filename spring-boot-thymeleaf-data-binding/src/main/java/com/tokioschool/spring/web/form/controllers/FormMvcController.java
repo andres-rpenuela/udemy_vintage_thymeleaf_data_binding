05ceceec7fe378b0f31bc72ab75350e4ac4d875f
Andrés Ruiz Peñuela <andres.rpenuela@babelgroup.com>
2024-10-22 09:21:21 +0200

    [udemy-sts6] validacion del objeto "user" recibio del formulario en el back

--- spring-boot-thymeleaf-data-binding/src/main/java/com/tokioschool/spring/web/form/controllers/FormMvcController.java
+++ spring-boot-thymeleaf-data-binding/src/main/java/com/tokioschool/spring/web/form/controllers/FormMvcController.java
@@ -1,7 +1,11 @@
 package com.tokioschool.spring.web.form.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tokioschool.spring.web.form.dto.UserFormDTO;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class FormMvcController {
 	
 	@GetMapping({"","/","/form"})
@@ -24,24 +32,42 @@ public class FormMvcController {
 			/*@RequestParam(name="username") String username,
 			@RequestParam String password,
 			@RequestParam("email") String emailUser*/
-			@ModelAttribute("userFormDto") UserFormDTO userFormDTO) {
+			@Valid @ModelAttribute("userFormDto") UserFormDTO userFormDTO, BindingResult result) {
 		
-		model.addAttribute("title", "Resultado");
-		
+		/** example 1 **/
 		// send to view param each
 		/*model.addAttribute("username", username);
 		model.addAttribute("password", password);
-		model.addAttribute("email", emailUser);*/
-		
+		model.addAttribute("email", emailUser);
+		return "result";	
+		*/
+		/** example 2 **/
 		// send to view the params wrappers in object
 		/*UserFormDTO userFormDTO = UserFormDTO.builder()
 				.name(username)
 				.email(emailUser)
 				.password(password)
-				.build();*/
-		
+				.build();
 		model.addAttribute("user", userFormDTO);
-		return "result";	
+		return "result";
+		*/	
+		/** example 3 **/
+		if(result.hasErrors()) {
+			Map<String,String> errors = new HashMap<>();
+			result.getFieldErrors().forEach(error->{
+				log.debug("process handler say: Error field %s".formatted(error.getField()));
+				errors.put(
+						error.getField(),
+						"El campo %s %s".formatted(error.getField(),error.getDefaultMessage())
+				);
+			});
+			model.addAttribute("title", "Formulario usuarios: Con errores");
+			model.addAttribute("error", errors);
+			return "form";
+		}
+		model.addAttribute("title", "Resultado");
+		model.addAttribute("user", userFormDTO);
+		return "result";
 	}
 
 }
--- spring-boot-thymeleaf-data-binding/src/main/java/com/tokioschool/spring/web/form/dto/UserFormDTO.java
+++ spring-boot-thymeleaf-data-binding/src/main/java/com/tokioschool/spring/web/form/dto/UserFormDTO.java
@@ -1,5 +1,6 @@
 package com.tokioschool.spring.web.form.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +11,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserFormDTO {
 	
+	@NotEmpty
 	private String name;
+	@NotEmpty
 	private String password;
+	@NotEmpty
 	private String email;
 
 }
